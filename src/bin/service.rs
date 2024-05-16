a0ecf49379416e1a80c91aa1dfd0812807643814
Bart Karalus <bart@bforge.co>
2024-05-16 16:44:35 +0200

    chore: auto-formatting; feat: apache-style storage dir listing

--- src/bin/service.rs
+++ src/bin/service.rs
@@ -1,14 +1,37 @@
-use std::{num::NonZeroUsize, sync::{Arc, Mutex}, thread, time::Duration};
-use tiny_http::{Server, Response}; 
 use link_parser::parse_yt_link;
 use std::sync::mpsc;
-
+use std::{
+    num::NonZeroUsize,
+    sync::{Arc, Mutex},
+    thread,
+    time::Duration,
+};
+use tiny_http::{Response, Server};
+use std::fs;
+use std::path::Path;
 use redis::Commands;
 
 extern crate redis;
 
 enum Message {
     NewJob(String),
+}
+
+fn generate_directory_listing(path: &Path) -> String {
+    let mut listing = String::new();
+    listing.push_str("<html><body><ul>");
+
+    if let Ok(entries) = fs::read_dir(path) {
+        for entry in entries {
+            if let Ok(entry) = entry {
+                let file_name = entry.file_name().into_string().unwrap();
+                listing.push_str(&format!("<li><a href=\"files/{}\">{}</a></li>", file_name, file_name));
+            }
+        }
+    }
+
+    listing.push_str("</ul></body></html>");
+    listing
 }
 
 fn main() {
@@ -24,69 +47,124 @@ fn main() {
 
         for mut request in server.incoming_requests() {
             match request.url() {
-                "/jobs" => {
-                    match request.method() {
-                        tiny_http::Method::Get => {
-                            match conn.try_lock() {
-                                Ok(mut conn) => {
-                                    let job_count = (*conn).llen::<&str, usize>(LIST_KEY).unwrap();
-                                    let response = Response::from_string(job_count.to_string());
-                                    request.respond(response).unwrap();
-                                },
-                                Err(_) => {
-                                    let error = Response::new(tiny_http::StatusCode(500), Vec::new(), "internal server error".as_bytes(), None, None);
-                                    request.respond(error).unwrap();
-                                }
-                            }
-                        },
-                        tiny_http::Method::Post => {
-                            let mut content = String::new();
-                            request.as_reader().read_to_string(&mut content).unwrap();
-                            
-                            match parse_yt_link(&content) {
-                                Some(video_id) => {
-                                    tx.send(Message::NewJob(video_id)).unwrap();
-                                    request.respond(Response::from_string("got it")).unwrap();
-                                },
-                                None => {
-                                    let error = Response::new(tiny_http::StatusCode(400), Vec::new(), "invalid link".as_bytes(), None, None);
-                                    request.respond(error).unwrap();
-                                }
-                            }
-                        },
-                        _ => {
-                            let error = Response::new(tiny_http::StatusCode(405), Vec::new(), "method not allowed".as_bytes(), None, None);
+                url if url.starts_with("/files") => match request.method() {
+                    tiny_http::Method::Get => {
+                        let url = request.url().trim_start_matches("/files");
+                        let url = format!("./storage{}", url);
+                        let path = Path::new(&url);
+                        println!("Request: {}", url);
+
+                        if path.is_dir() {
+                            let dir_listing = generate_directory_listing(path);
+                            let response = Response::from_string(dir_listing).with_header(
+                                "Content-Type: text/html".parse::<tiny_http::Header>().unwrap()
+                            );
+                            request.respond(response).unwrap();
+                        } else if path.is_file() {
+                            let file_content = fs::read(path).unwrap();
+                            let response = Response::from_data(file_content).with_header(
+                                "Content-Type: application/octet-stream".parse::<tiny_http::Header>().unwrap()
+                            );
+                            request.respond(response).unwrap();
+                        } else {
+                            let response = Response::from_string("404 Not Found")
+                                .with_status_code(404)
+                                .with_header("Content-Type: text/plain".parse::<tiny_http::Header>().unwrap());
+                            request.respond(response).unwrap();
+                        }
+                    },
+                    _ => {
+                        let error = Response::new(
+                            tiny_http::StatusCode(405),
+                            Vec::new(),
+                            "method not allowed".as_bytes(),
+                            None,
+                            None,
+                        );
+                        request.respond(error).unwrap();
+                    }
+                },
+                "/jobs" => match request.method() {
+                    tiny_http::Method::Get => match conn.try_lock() {
+                        Ok(mut conn) => {
+                            let job_count = (*conn).llen::<&str, usize>(LIST_KEY).unwrap();
+                            let response = Response::from_string(job_count.to_string());
+                            request.respond(response).unwrap();
+                        }
+                        Err(_) => {
+                            let error = Response::new(
+                                tiny_http::StatusCode(500),
+                                Vec::new(),
+                                "internal server error".as_bytes(),
+                                None,
+                                None,
+                            );
                             request.respond(error).unwrap();
-                        },
+                        }
+                    },
+                    tiny_http::Method::Post => {
+                        let mut content = String::new();
+                        request.as_reader().read_to_string(&mut content).unwrap();
+
+                        match parse_yt_link(&content) {
+                            Some(video_id) => {
+                                tx.send(Message::NewJob(video_id)).unwrap();
+                                request.respond(Response::from_string("got it")).unwrap();
+                            }
+                            None => {
+                                let error = Response::new(
+                                    tiny_http::StatusCode(400),
+                                    Vec::new(),
+                                    "invalid link".as_bytes(),
+                                    None,
+                                    None,
+                                );
+                                request.respond(error).unwrap();
+                            }
+                        }
+                    }
+                    _ => {
+                        let error = Response::new(
+                            tiny_http::StatusCode(405),
+                            Vec::new(),
+                            "method not allowed".as_bytes(),
+                            None,
+                            None,
+                        );
+                        request.respond(error).unwrap();
                     }
                 },
                 _ => {
-                    let error = Response::new(tiny_http::StatusCode(404), Vec::new(), "not found".as_bytes(), None, None);
+                    let error = Response::new(
+                        tiny_http::StatusCode(404),
+                        Vec::new(),
+                        "not found".as_bytes(),
+                        None,
+                        None,
+                    );
                     request.respond(error).unwrap();
-                },
+                }
             }
-          }
+        }
     });
 
     loop {
         match rx.try_recv() {
-            Ok(result) => {
-                match result {
-                    Message::NewJob(video_id) => {
-                        match conn2.try_lock() {
-                            Ok(mut conn) => {
-                                conn.lpush::<&str, &str, i8>(LIST_KEY, video_id.as_str()).unwrap();
-                                println!("New job: {}", video_id);
-                            },
-                            Err(_) => {
-                                println!("Failed to lock redis connection");
-                            }
-                        };
-                    },
+            Ok(result) => match result {
+                Message::NewJob(video_id) => {
+                    match conn2.try_lock() {
+                        Ok(mut conn) => {
+                            conn.lpush::<&str, &str, i8>(LIST_KEY, video_id.as_str())
+                                .unwrap();
+                            println!("New job: {}", video_id);
+                        }
+                        Err(_) => {
+                            println!("Failed to lock redis connection");
+                        }
+                    };
                 }
             },
             Err(_) => {
-                
                 match conn2.try_lock() {
                     Ok(mut conn) => {
                         let job_count = conn.llen::<&str, usize>(LIST_KEY).unwrap();
@@ -99,18 +177,16 @@ fn main() {
                                 Err(err) => println!("Failed to fetch next job info: {}", err),
                             }
                         }
-                
-                        println!("Current Job count is: {}", job_count);  
-                    },
+
+                        println!("Current Job count is: {}", job_count);
+                    }
                     Err(_) => {
                         println!("Failed to lock redis connection");
                     }
                 };
-  
+
                 thread::sleep(Duration::from_secs(1));
-            },
+            }
         }
     }
 }
-
-
